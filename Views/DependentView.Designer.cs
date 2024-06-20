1690275125378d04dd538aceecd4a3ededc82b4d
Guilherme Siqueira <siqueiraguilherme920@gmail.com>
2024-06-20 00:49:59 -0300

    feat: Added dependent view

---
+++ Views/DependentView.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace GerenciamentoClubesEsportivos.Views
+{
+    partial class DependentView
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            EditButton = new Button();
+            InputName = new TextBox();
+            Table = new DataGridView();
+            InputSearch = new TextBox();
+            LabelName = new Label();
+            SearchButton = new Button();
+            DismissButton = new Button();
+            InputCPF = new TextBox();
+            DeleteButton = new Button();
+            SaveButton = new Button();
+            LabelEmail = new Label();
+            InputKinship = new TextBox();
+            label2 = new Label();
+            InputMemberID = new TextBox();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)Table).BeginInit();
+            SuspendLayout();
+            // 
+            // EditButton
+            // 
+            EditButton.Enabled = false;
+            EditButton.Location = new Point(83, 340);
+            EditButton.Name = "EditButton";
+            EditButton.Size = new Size(75, 23);
+            EditButton.TabIndex = 40;
+            EditButton.Text = "Editar";
+            EditButton.UseVisualStyleBackColor = true;
+            EditButton.Visible = false;
+            EditButton.Click += EditButton_Click;
+            // 
+            // InputName
+            // 
+            InputName.Location = new Point(83, 54);
+            InputName.Name = "InputName";
+            InputName.PlaceholderText = "Emma Lazarus";
+            InputName.Size = new Size(240, 23);
+            InputName.TabIndex = 37;
+            // 
+            // Table
+            // 
+            Table.AllowUserToAddRows = false;
+            Table.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            Table.Location = new Point(358, 54);
+            Table.Name = "Table";
+            Table.RowHeadersWidth = 51;
+            Table.Size = new Size(616, 428);
+            Table.TabIndex = 33;
+            // 
+            // InputSearch
+            // 
+            InputSearch.Location = new Point(358, 16);
+            InputSearch.Name = "InputSearch";
+            InputSearch.PlaceholderText = "John Doe";
+            InputSearch.Size = new Size(273, 23);
+            InputSearch.TabIndex = 39;
+            // 
+            // LabelName
+            // 
+            LabelName.AutoSize = true;
+            LabelName.Location = new Point(27, 58);
+            LabelName.Name = "LabelName";
+            LabelName.Size = new Size(40, 15);
+            LabelName.TabIndex = 27;
+            LabelName.Text = "Nome";
+            // 
+            // SearchButton
+            // 
+            SearchButton.Location = new Point(635, 16);
+            SearchButton.Name = "SearchButton";
+            SearchButton.Size = new Size(75, 23);
+            SearchButton.TabIndex = 29;
+            SearchButton.Text = "Buscar";
+            SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // DismissButton
+            // 
+            DismissButton.Location = new Point(248, 294);
+            DismissButton.Name = "DismissButton";
+            DismissButton.Size = new Size(75, 23);
+            DismissButton.TabIndex = 30;
+            DismissButton.Text = "Cancelar";
+            DismissButton.UseVisualStyleBackColor = true;
+            DismissButton.Click += DismissButton_Click;
+            // 
+            // InputCPF
+            // 
+            InputCPF.Location = new Point(83, 99);
+            InputCPF.Name = "InputCPF";
+            InputCPF.PlaceholderText = "000.000.000-00";
+            InputCPF.Size = new Size(240, 23);
+            InputCPF.TabIndex = 35;
+            // 
+            // DeleteButton
+            // 
+            DeleteButton.Enabled = false;
+            DeleteButton.Location = new Point(248, 340);
+            DeleteButton.Name = "DeleteButton";
+            DeleteButton.Size = new Size(75, 23);
+            DeleteButton.TabIndex = 31;
+            DeleteButton.Text = "Deletar";
+            DeleteButton.UseVisualStyleBackColor = true;
+            DeleteButton.Visible = false;
+            DeleteButton.Click += DeleteButton_Click;
+            // 
+            // SaveButton
+            // 
+            SaveButton.Location = new Point(83, 294);
+            SaveButton.Name = "SaveButton";
+            SaveButton.Size = new Size(75, 23);
+            SaveButton.TabIndex = 32;
+            SaveButton.Text = "Adicionar";
+            SaveButton.UseVisualStyleBackColor = true;
+            SaveButton.Click += SaveButton_Click;
+            // 
+            // LabelEmail
+            // 
+            LabelEmail.AutoSize = true;
+            LabelEmail.Location = new Point(31, 103);
+            LabelEmail.Name = "LabelEmail";
+            LabelEmail.Size = new Size(28, 15);
+            LabelEmail.TabIndex = 25;
+            LabelEmail.Text = "CPF";
+            // 
+            // InputKinship
+            // 
+            InputKinship.Location = new Point(83, 187);
+            InputKinship.Name = "InputKinship";
+            InputKinship.PlaceholderText = "Pai";
+            InputKinship.Size = new Size(240, 23);
+            InputKinship.TabIndex = 42;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 191);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 15);
+            label2.TabIndex = 41;
+            label2.Text = "Parentesco";
+            // 
+            // InputMemberID
+            // 
+            InputMemberID.Location = new Point(83, 142);
+            InputMemberID.Name = "InputMemberID";
+            InputMemberID.PlaceholderText = "0000";
+            InputMemberID.Size = new Size(240, 23);
+            InputMemberID.TabIndex = 44;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(14, 146);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 15);
+            label3.TabIndex = 43;
+            label3.Text = "ID Membro";
+            // 
+            // DependentView
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(InputMemberID);
+            Controls.Add(label3);
+            Controls.Add(InputKinship);
+            Controls.Add(label2);
+            Controls.Add(EditButton);
+            Controls.Add(InputName);
+            Controls.Add(Table);
+            Controls.Add(InputSearch);
+            Controls.Add(LabelName);
+            Controls.Add(SearchButton);
+            Controls.Add(DismissButton);
+            Controls.Add(InputCPF);
+            Controls.Add(DeleteButton);
+            Controls.Add(SaveButton);
+            Controls.Add(LabelEmail);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "DependentView";
+            Size = new Size(990, 498);
+            ((System.ComponentModel.ISupportInitialize)Table).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button EditButton;
+        private TextBox InputName;
+        private DataGridView Table;
+        private TextBox InputSearch;
+        private Label LabelName;
+        private Button SearchButton;
+        private Button DismissButton;
+        private TextBox InputCPF;
+        private Button DeleteButton;
+        private Button SaveButton;
+        private Label LabelEmail;
+        private TextBox InputKinship;
+        private Label label2;
+        private TextBox InputMemberID;
+        private Label label3;
+    }
+}
---
+++ Views/DependentView.cs
@@ -0,0 +1,163 @@
+﻿using GerenciamentoClubesEsportivos.Controllers;
+using GerenciamentoClubesEsportivos.Models.Entities;
+using GerenciamentoClubesEsportivos.Models.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace GerenciamentoClubesEsportivos.Views
+{
+    public partial class DependentView : UserControl
+    {
+        private readonly DependentController controller;
+        private BindingList<Dependent>? bindingDependents;
+        private string selectedDependentId;
+
+        private string? name;
+        private string? cpf;
+        private string? kinship;
+        private string? memberId;
+
+        public string IName
+        {
+            get { return name!; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string CPF
+        {
+            get { return cpf!; }
+            set
+            {
+                cpf = value;
+                OnPropertyChanged("CPF");
+            }
+        }
+        public string Kinship
+        {
+            get { return kinship!; }
+            set
+            {
+                kinship = value;
+                OnPropertyChanged("Kinship");
+            }
+        }
+        public string MemberID
+        {
+            get { return memberId!; }
+            set
+            {
+                memberId = value;
+                OnPropertyChanged("MemberID");
+            }
+        }
+        public DependentView(DependentController controller)
+        {
+            this.controller = controller;
+
+            InitializeComponent();
+            BindProperties();
+            UpdateDataGridView(this.controller.GetAllDependents());
+        }
+
+        private void BindProperties()
+        {
+            InputName.DataBindings.Add("Text", this, "IName");
+            InputCPF.DataBindings.Add("Text", this, "CPF");
+            InputKinship.DataBindings.Add("Text", this, "Kinship");
+            InputMemberID.DataBindings.Add("Text", this, "MemberID");
+        }
+
+        private void UpdateDataGridView(List<Dependent> dependents)
+        {
+            bindingDependents = new BindingList<Dependent>(dependents);
+            Table.DataSource = bindingDependents.Select(dependent =>
+                new { dependent.Id, dependent.Name, dependent.kinship }
+            ).ToList();
+            Table.Refresh();
+        }
+
+        //metodos de handle de eventos
+        private void SaveButton_Click(Object sender, EventArgs e)
+        {
+            controller.AddDependent(IName, CPF, Kinship, MemberID);
+            UpdateDataGridView(controller.GetAllDependents());
+            Clear();
+        }
+        private void DismissButton_Click(object sender, EventArgs e)
+        {
+            Clear();
+            Table.ClearSelection();
+            DisableUpdateOrDelete();
+        }
+        private void EditButton_Click(Object sender, EventArgs e)
+        {
+            DependentController controllerx = new DependentController(new DependentRepository());
+
+            MessageBox.Show("xx5");
+            foreach (var item in controllerx.GetAllDependents())
+            {
+                MessageBox.Show(item.Name + item.Id);
+            }
+            Dependent dependent = controller.GetDependentByID(selectedDependentId);
+
+            controller.UpdateDependent(selectedDependentId, IName, CPF, Kinship, dependent.memberId);
+            UpdateDataGridView(controller.GetAllDependents());
+            Clear();
+        }
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            Dependent dependent = controller.GetDependentByID(selectedDependentId);
+            controller.DeleteDependent(selectedDependentId);
+            UpdateDataGridView(controller.GetAllDependents());
+        }
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            string dependentName = InputSearch.Text;
+            UpdateDataGridView(controller.SearchByName(dependentName));
+        }
+
+        //metodos adicionais/dependentes
+        private void Clear()
+        {
+            InputName.Text =
+            InputKinship.Text =
+            InputMemberID.Text =
+            InputCPF.Text = "";
+        }
+        private void EnableUpdateOrDelete()
+        {
+            SaveButton.Enabled = false;
+            DeleteButton.Visible = true;
+            DeleteButton.Enabled = true;
+            EditButton.Visible = true;
+            EditButton.Enabled = true;
+        }
+        private void DisableUpdateOrDelete()
+        {
+            SaveButton.Enabled = true;
+            DeleteButton.Visible = false;
+            DeleteButton.Enabled = false;
+            EditButton.Visible = false;
+            EditButton.Enabled = false;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
